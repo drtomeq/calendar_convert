df7bc99d77c1a5753d3fc867fcd1511f63dd3257
drtomeq <quinnte2@yahoo.com>
2023-09-21 14:11:18 +0100

    Fixed DST issues

--- calendarConvert datetime.py
+++ calendarConvert datetime.py
@@ -1,52 +1,78 @@
 """imports Google calendar data and convert and export into format for spreadsheets.
 
 to be used for invoices tax returns etc
-Sadly what is missing in the Google calendar expert are the colours
+Sadly what is missing in the Google calendar export are the colours
 So it might not be able to identify red canceled sessions
 or sessions that are yellow maybe
 """
 import datetime
 # use relativedelta as it allows to add 1 year
 from dateutil.relativedelta import relativedelta
-now = datetime.datetime.now()
 
+# adjust times for BST
+# ZoneInfo only works if IANA time database is included in system
+# tzdata needed if no database on system
+import tzdata
+from zoneinfo import ZoneInfo
+now = datetime.datetime.now()
+mytz = ZoneInfo("Europe/London")
 
 # can't use 0 in datetime so initialise to 1
 # make a global version here so can get suggestions 
-init_dt = datetime.datetime(1,1,1)
-my_dt = datetime.datetime(1,1,1)
+init_dt = datetime.datetime(1,1,1, tzinfo=mytz)
+print(init_dt)
+# my_dt = datetime.datetime(1,1,1, tzinfo=datetime.timezone.utc)
+# tzdt = datetime.datetime(2023,1,19, tzinfo=ZoneInfo("Europe/London"))
 
-
-# make sure you check the file names before running
-# it will overwrite existing data so be careful
 
 # the calendar file as exported from Google calendar
+# change to the appropriate file name
+# make sure you check the file names before running
+# it will overwrite existing data so be careful
 calendar_file = open("londonmathstuitionAug2023.txt", "r")
 # the output file which will have the data in a 
 # spreadsheet friendly format
 output_file = open("calendar2023Aug.csv", "w")
 
-# each event in the calendar
+# each event in the calendar, a global variable
 events = []
 
 # while there are other categories, these are the ones we are intersted in
 # categories = ["name","start","end","stop repeat","exclusions"]
 
-
 def find_number_start(line):
+    '''Find the first character that is a number
+    
+    used to find where datetimes are in the source text'''
     for pos, val in enumerate(line):
         if val.isdecimal():
             start_pos = pos
             return pos 
+
+def DSTadjust(dt: datetime):
+    '''Change for daylight saving time
+    
+    this was a longer function, but it was uneccesary 
+    as time zones can correct for seasonal changes'''
+
+    '''
+    print("before", dt)
+    tzdt = dt.replace(tzinfo=ZoneInfo("Europe/London"))
+    tzdt = tzdt.replace(hour=((tzdt.hour + (tzdt.dst()).seconds//3600))%24)
+    tzdt = dt.replace(tzinfo=datetime.timezone.utc)
+    '''
+    tzdt = dt.astimezone(mytz)
+    # print("after", tzdt)
+    return tzdt
 
 def read_date_time(line):
     """Get one date or date and time value
         
     Assume that the date is the first number on the line
     Assume it is the standard format of 
-    4 digit year number
-    2 digit month number
-    2 digit day date number
+        * 4 digit year number
+        * 2 digit month number
+        * 2 digit day date number
     If an all day event it will finish with this
     If it has a time it will follow straight after
     Start with the letter T
@@ -89,6 +115,8 @@ def read_date_time(line):
         print(line)
         datetime = {}
 
+    my_dt = DSTadjust(my_dt)
+
     return my_dt
 
 def get_freq(line):
@@ -120,12 +148,12 @@ def get_one_data():
 
     Can't indetify item by position as it is inconsistant
     so we look for an identifier
-    Assume it ends with END:VEVENT
     Look for start date DTSTART
+    Assume it ends with END:VEVENT
     end date DTEND
     exlusions EXDATE
     when we stop repeating RRULE (also contains frequency)
-    the client name SUMMARY
+    the task or client name SUMMARY
     We ignore everything else
     We keep getting the information
     Add to a dictionary
@@ -149,6 +177,7 @@ def get_one_data():
             # otherwise get it to repeat until current day
             else:
                 event["stop repeat"] = datetime.datetime.now()
+                event["stop repeat"].replace(tzinfo=datetime.timezone.utc)
             event["freq"] = get_freq(line)
         elif line[:7] == "SUMMARY":
             event["name"] = line[8:]
@@ -157,10 +186,10 @@ def get_one_data():
     
 
 def read_calendar():
-    """
-        read the whole calendar file
-        assume that every event begins with BEGIN:VEVENT
-        and the callendar ends with END:VCALENDAR
+    """read the whole calendar file and add to events
+    
+    assume that every event begins with BEGIN:VEVENT
+    and the callendar ends with END:VCALENDAR
     """
     global events
     while True:
@@ -170,7 +199,6 @@ def read_calendar():
         if line == "BEGIN:VEVENT\n":
             events.append(get_one_data())
     calendar_file.close()
-    return 
 
 def repeated_events():
     """If repeated, add items to event list for each time its repeated
@@ -190,9 +218,10 @@ def repeated_events():
         # all repeated events should have a stop repeat
         # if it didn't, it should have been assigned now earlier
         try: 
-        # we do equality to raise if stop repeat not exists
+        # we do equality to raise exception if stop repeat does not exists
             event["stop repeat"] = event["stop repeat"]
         except(KeyError):
+            # if there is no stop repeat, skip it go to next event
             continue
         stop_dt = event["stop repeat"]
         new_event = event.copy()
@@ -205,7 +234,7 @@ def repeated_events():
         elif event["freq"] == "daily":
             delta = datetime.timedelta(days=1)
         else:
-            print("******* not the write frequency ********")
+            print("******* not the right frequency ********")
             print("freq = ", event["freq"])
             continue
 
@@ -213,7 +242,7 @@ def repeated_events():
             new_event["start"] = new_event["start"] + delta 
             new_event["end"] = new_event["end"] + delta
 
-            if new_event["end"] > stop_dt:
+            if (new_event["end"]).timestamp() > stop_dt.timestamp():
                 break
             if new_event["start"] in new_event["exclusions"]:
                 new_event["exclusions"].remove(new_event["start"]) 
@@ -269,4 +298,4 @@ repeated_events()
 # write_unformated()
 write_calendar()
 
-print(events)
+# print(events)
